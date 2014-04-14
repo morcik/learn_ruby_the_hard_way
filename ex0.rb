47480b5bf3dfdd348b2eb282a7de4146aade8bd5
Morcik <b.truchel@gmail.com>
2014-04-14 13:04:12 -0700

    lesson 1

---
+++ ex0.rb
@@ -0,0 +1,6 @@
+puts "hello World!"
+puts "Hello again"
+puts "I like typing this"
+puts "o what the hell!"
+puts "let's try the single quotes inside double quotes"
+puts 'or maybe "double quotes" in single quotes.'
